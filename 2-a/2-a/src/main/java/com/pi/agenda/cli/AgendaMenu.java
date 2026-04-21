f9a0babb34ba8777354225d29b88bf95d3be2e88
Rikelmy Matos <rikelmymatos07@gmail.com>
2026-04-21 15:10:36 -0300

    fix: substitui data.sql por DataSeeder programatico para seeding confiavel
    - Remove data.sql e spring.sql.init.mode (nao funciona com banco nao-embutido)
    - Cria DataSeeder com @Order(1) que verifica existencia antes de inserir (idempotente)
    - Adiciona @Order(2) ao AgendaMenu para garantir execucao apos o seed

--- 2-a/2-a/src/main/java/com/pi/agenda/cli/AgendaMenu.java
+++ 2-a/2-a/src/main/java/com/pi/agenda/cli/AgendaMenu.java
@@ -5,6 +5,7 @@ import com.pi.agenda.service.AgendaTelefonica;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.Scanner;
 
 @Slf4j
 @Component
+@Order(2)
 @RequiredArgsConstructor
 public class AgendaMenu implements CommandLineRunner {
 
